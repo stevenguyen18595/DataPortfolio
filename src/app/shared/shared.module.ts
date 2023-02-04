dd99b59989b007987ec1fc656ad543384f2ba0b9
NTI\nguyens <stevenguyen@nti.com.au>
2023-02-04 23:35:42 +1000

    add avatar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [MatSidenavModule, MatIconModule],
+  declarations: [HeaderComponent, AvatarComponent],
+  imports: [MatSidenavModule, MatIconModule, MatDividerModule],
   providers: [],
-  exports: [MatSidenavModule, MatIconModule, HeaderComponent],
+  exports: [MatSidenavModule, MatIconModule, HeaderComponent, AvatarComponent],
 })
 export class SharedModule {}
