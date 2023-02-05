c905895683e880754a3a20b3a14b01f8578134f9
NTI\nguyens <stevenguyen@nti.com.au>
2023-02-05 16:36:30 +1000

    add routing to nav bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { AvatarComponent } from './avatar/avatar.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { SideNavigationPanelComponent } from './side-navigation-panel/side-navigation-panel.component';
 import { SideNavigationSectionComponent } from './side-navigation-panel/side-navigation-section/side-navigation-section.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { SideNavigationSectionComponent } from './side-navigation-panel/side-nav
     SideNavigationPanelComponent,
     SideNavigationSectionComponent,
   ],
-  imports: [MatSidenavModule, MatIconModule, MatDividerModule],
+  imports: [CommonModule, MatSidenavModule, MatIconModule, MatDividerModule],
   providers: [],
   exports: [
+    CommonModule,
     MatSidenavModule,
     MatIconModule,
     HeaderComponent,
--- src/app/shared/side-navigation-panel/side-navigation-section/side-navigation-section.component.ts
+++ src/app/shared/side-navigation-panel/side-navigation-section/side-navigation-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-navigation-section',
@@ -8,5 +9,16 @@ import { Component, Input } from '@angular/core';
 export class SideNavigationSectionComponent {
   @Input() matIconName?: string;
   @Input() sectionName?: string;
-  @Input() route?: string;
+  @Input() route!: string;
+
+  constructor(private router: Router) {}
+
+  navigateToPage(route: string) {
+    this.router.navigateByUrl(route);
+  }
+
+  get isActive() {
+    const currentRoute = this.router.url;
+    return currentRoute.includes(this.route);
+  }
 }
